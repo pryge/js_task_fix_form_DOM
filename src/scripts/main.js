ac7885d3f6a3802a572a67eec2d8898efd77a6cd
Oleh <pryge321@gmail.com>
2025-02-12 21:49:32 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   const inputItems = document.querySelectorAll('form input');
 
+  for (let i = 0; i < inputItems.length; i++) {
+    const items = inputItems[i];
+
+    if (!items.id) {
+      items.id = 'input' + (i + 1);
+    }
+  }
+
   for (const item of inputItems) {
     const label = document.createElement('label');
 
